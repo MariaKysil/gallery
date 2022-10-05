bdee5009eb161e5f89ff97f74fe41c1daf15996e
Mariia Kysil <77671872+MariaKysil@users.noreply.github.com>
2022-10-05 13:20:42 +0300

    gallery amrkup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
+import * as basicLightbox from 'basiclightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const imgMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', imgMarkup);
+
+galleryContainer.addEventListener('click', onImgClick);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+<a class="gallery__link" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a>`
+    )
+    .join('');
+}
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" alt ="${e.target.description}" width="800" height="600">
+  `);
+  instance.show();
+
+  galleryContainer.addEventListener('keydown', e => {
+    if (e.code === 'Escape') return instance.close();
+  });
+}
